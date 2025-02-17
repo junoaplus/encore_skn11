9216162df02ab8bc3e9d8d4547806c82dce57a9d
junoaplus <junehoo4248@naver.com>
2025-02-17 09:08:36 +0900

    2주차 완료

---
+++ python-basic/07_module/our_class_dir/unofficial/unofficial_module.py
@@ -0,0 +1,11 @@
+import random
+from our_class_dir.official.official_moule import student_count
+
+
+def study():
+    print(f"{student_count}명의 학생들이 열심히 공부를 한다!")
+    
+    
+def go_lunch(menus):
+    return random.choice(menus)
+    
