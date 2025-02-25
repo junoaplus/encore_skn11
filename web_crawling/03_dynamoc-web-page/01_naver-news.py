38c805b676f5189cd891fc4bca4a7e50a948db86
junoaplus <junehoo4248@naver.com>
2025-02-25 15:53:48 +0900

    확인

--- web_crawling/03_dynamoc-web-page/01_naver-news.py
+++ web_crawling/03_dynamoc-web-page/01_naver-news.py
@@ -21,32 +21,68 @@ time.sleep(1)
 
 # 3. 검색 처리
 search_box = driver.find_element(By.ID, "query") 
-search_box.send_keys('chat gpt')
+search_box.send_keys('llm')
 search_box.send_keys(Keys.RETURN)
 time.sleep(1)
 
 # - 뉴스 탭 이동
-news_btn = driver.find_element(By.XPATH, '//*[@id="lnb"]/div[1]/div/div[1]/div/div[1]/div[8]/a')
+news_btn = driver.find_element(By.XPATH, '//*[@id="lnb"]/div[1]/div/div[1]/div/div[1]/div[1]/a')
 news_btn.click()
 time.sleep(3)
 
 # 4. 스코를 처리
-for _ in range(5):
+for _ in range(1):
     body = driver.find_element(By.TAG_NAME, "body")
     body.send_keys(Keys.END)
     time.sleep(1)
     
 # 5. 특정 요소에 접근
-news_contents_elems = driver.find_elements(By.CSS_SELECTOR, ".news_contents")
-print(len(news_contents_elems))
+news_area_elems = driver.find_elements(By.CSS_SELECTOR, ".news_area")
+#news_info_elem = driver.find_elements(By.CSS_SELECTOR, ".news_info")
+#news_contents_elems = driver.find_elements(By.CSS_SELECTOR, ".news_contents")
+news_data = []
+print(len(news_area_elems))
 
-for news_contents_elem in news_contents_elems:
+for news_area_elem in news_area_elems:
     #print(news_contents_elem)
     #print(type(news_contents_elem))
-    a_tag = news_contents_elem.find_element(By.CSS_SELECTOR, ".news_tit")
-    title = a_tag.text 
-    href = a_tag.get_attribute('href')
-    print(title, '|', href)
+    news_info_elem = news_area_elem.find_element(By.CSS_SELECTOR, ".news_info")
+    news_contents_elem = news_area_elem.find_element(By.CSS_SELECTOR, ".news_contents")
     
+    info_elem = news_info_elem.find_elements(By.CSS_SELECTOR, "a.info")
+    
+    if len(info_elem) > 1 and '네이버뉴스' in info_elem[1].text:
+        
+        a_tag = news_contents_elem.find_element(By.CSS_SELECTOR, ".news_tit")
+        title = a_tag.text 
+        href = a_tag.get_attribute('href')
+        print(title, '|', href)
+        
+        driver.execute_script('window.open("");')
+        driver.switch_to.window(driver.window_handles[1])
+        driver.get(href)
+        time.sleep(1)
+        
+        content = driver.find_element(By.TAG_NAME, "div").text
+        
+        news_data.append({
+            "title" : title,
+            "link" : href,
+            "content" : content
+        })
+        
+        # driver.back()
+        driver.close()
+        
+        driver.switch_to.window(driver.window_handles[0])
+        time.sleep(1)
+        
+with  open('news_data.txt', 'w', encoding='utf8') as file:
+    for news in news_data:
+        file.write(f'제목: {news['title']}\n')
+        file.write(f'링크: {news['link']}\n')
+        file.write(f'내용: {news['content']}\n')
+        file.write("="*100)
+        file.write('\n')
     
 driver.quit()
